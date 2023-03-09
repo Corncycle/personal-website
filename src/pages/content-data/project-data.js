552f3a1a482003c9ea73d13c21efec0dc82966e0
Caleb Stromberg <calebstromberg@gmail.com>
2023-03-09 13:03:18 -0800

    Touch up projects page

--- src/pages/content-data/project-data.js
+++ src/pages/content-data/project-data.js
@@ -7,7 +7,7 @@ const projectData = [
     img: findTheFighterImg,
     name: 'Find The Fighter',
     description:
-      'An image searching game bootstrapped by create-react-app. High scores are stored and retrieved from a Firebase Firestore database to be compared online.',
+      'An image searching game bootstrapped by create-react-app. High scores are stored and retrieved from a Firestore database to be compared online.',
     demoLink: 'https://corncycle.github.io/find-the-fighter/',
     sourceLink: 'https://github.com/Corncycle/find-the-fighter',
   },
@@ -15,7 +15,7 @@ const projectData = [
     img: smashMemoryImg,
     name: 'Smash Memory',
     description:
-      'A browser game to test memory of previously selected Smash Bros. fighters. High scores are stored locally using the localStorage api. Assets were scraped from smashbros.com using axios in Node.js',
+      'An in-browser game to test memory of previously selected Smash Bros. fighters. High scores are stored using the localStorage API. Assets were scraped from smashbros.com using axios in Node.js',
     demoLink: 'https://corncycle.github.io/memory-game/',
     sourceLink: 'https://github.com/Corncycle/memory-game',
   },
@@ -23,7 +23,7 @@ const projectData = [
     img: personalWebsiteImg,
     name: 'Personal Website',
     description:
-      'The site you are currently on! This site was made in React, with client-side routing from React Router. It also has responsive design with a collapsible nav bar for mobile.',
+      'The site you are currently on! This site was made in React, with client-side routing from React Router. It has a responsive design with a collapsible nav bar for mobile.',
     demoLink: 'https://www.calebstromberg.com/',
     sourceLink: 'https://github.com/Corncycle/personal-website',
   },
